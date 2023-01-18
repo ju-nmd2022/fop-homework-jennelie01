465a259f617d036575cda09a6b7953357babe797
jennelie01 <coje22ad@student.ju.se>
2023-01-18 10:28:55 +0100

    initial commit

---
+++ emoji.js
@@ -0,0 +1,25 @@
+background(255, 255, 255);
+
+push();
+translate(150, 100);
+
+// yellow body
+fill(255, 255, 0);
+ellipse(150, 150, 200);
+
+// eyebrows
+
+// eyes and mouth
+fill(0, 0, 0);
+ellipse(115, 130, 50);
+ellipse(185, 130, 50);
+ellipse(150, 190, 35);
+
+// white reflection in eyes
+fill(255, 255, 255);
+ellipse(110, 122, 27);
+ellipse(124, 140, 10);
+ellipse(180, 122, 27);
+ellipse(194, 140, 10);
+
+pop();
